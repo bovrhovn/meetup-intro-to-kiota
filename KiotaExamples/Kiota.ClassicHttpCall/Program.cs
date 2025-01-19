76cfb2ccf0ed4f0dd4c56be559315009e4d2bbf5
Bojan Vrhovnik <bojan@vrhovnik.net>
2025-01-19 21:00:46 +0100

    classical http call to categories and getting the data

---
+++ KiotaExamples/Kiota.ClassicHttpCall/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+Console.WriteLine("Calling HTTP classical way!");
+var client = new HttpClient();
+client.BaseAddress = new Uri("https://localhost:5008");
+var response = await client.GetAsync("categories/all");
+response.EnsureSuccessStatusCode();
+Console.WriteLine("Receiving response....");
+var content = await response.Content.ReadAsStringAsync();
+Console.WriteLine("Found the following >>>> ");
+if (!string.IsNullOrEmpty(content))
+{
+    //deserialize the content to list of category records
+    var categories = JsonSerializer.Deserialize<List<CategoryModel>>(content);
+    ArgumentNullException.ThrowIfNull(categories);
+    foreach (var category in categories)
+    {
+        Console.WriteLine($"Category Id: {category.CategoryId}, Name: {category.Name}");
+    }
+}
+else
+{
+    Console.WriteLine("No results found");
+}
+
+record CategoryModel(string CategoryId, string Name);
