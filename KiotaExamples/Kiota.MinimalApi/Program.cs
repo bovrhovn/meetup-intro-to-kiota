94d28849aaff67316f2e394eb066b60d2178bff1
Bojan Vrhovnik <bojan@vrhovnik.net>
2025-01-21 10:26:36 +0100

    adding document OpenApi document transformer to get additional information to Kiota

--- KiotaExamples/Kiota.MinimalApi/Program.cs
+++ KiotaExamples/Kiota.MinimalApi/Program.cs
@@ -4,6 +4,7 @@ using Kiota.MinimalApi.Options;
 using Kiota.MinimalApi.Services;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.HttpOverrides;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOptions<WebAppOptions>().Bind(builder.Configuration.GetSection("WebAppOptions"));
@@ -14,12 +15,35 @@ builder.Services.AddMemoryCache();
 builder.Services.AddHealthChecks();
 builder.Services.AddSingleton<CategoryServiceInMemory>();
 builder.Services.AddSingleton<ProjectServiceInMemory>();
-    
-builder.Services.AddOpenApi();
+builder.Services.AddTransient<ILogger>(p =>
+{
+    var loggerFactory = p.GetRequiredService<ILoggerFactory>();
+    return loggerFactory.CreateLogger("Kiota.MinimalApi");
+});
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer((document, context, cancellationToken) =>
+    {
+        document.Info = new()
+        {
+            Title = "Kiota Example API",
+            Version = "v1",
+            Description = "API for demo purposes to understand Kiota better.",
+            Contact = new()
+            {
+                Name = "Kiota Demo App",
+                Email = "kiota@thisiscool.com"
+            }
+        };
+        return Task.CompletedTask;
+    });
+});
 
 var app = builder.Build();
 
 app.MapOpenApi();
+// to view scalar data go to https://localhost:<port>/scalar/v1
+app.MapScalarApiReference();
 
 app.MapGroup(RouteHelper.CategoryControllerRoute)
     .MapCategoriesApi()
